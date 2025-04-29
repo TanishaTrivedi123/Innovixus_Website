1e5b2804ded251b6d36faa228721b51c6b7b334e
TanishaTrivedi <tanisha6704@gamil.com>
2025-04-29 15:40:07 +0530

    Radhe Radhe

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,6 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
-app.use("/uploads", express.static("uploads"));
-
 // Test route
 app.get("/", (req, res) => {
   res.send("Jai Shree Krishn");
